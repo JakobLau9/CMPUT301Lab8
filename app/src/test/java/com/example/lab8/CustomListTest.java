e51ba0ac4faa4902d0dddaf1f2df8b85bda9a541
JakobLau9 <JakobLau9@gmail.com>
2022-10-26 17:40:47 -0600

    Added deleteCity test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -51,6 +51,7 @@ public class CustomListTest {
         assertEquals(false, test2);
     }
 
+    @Test
     void testDelete(){
         list = MockCityList();
         City city = new City("Regina", "Saskatchewan");
